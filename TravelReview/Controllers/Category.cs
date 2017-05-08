35e37124d65428610808b422fd2fd4da401bdee8
shirah <shirah.davis@gmail.com>
2017-05-08 06:59:16 -0400

    initial commit

---
+++ TravelReview/Controllers/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+//added this model
+
+namespace TravelReview.Controllers
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        //[Display(Name = "Category Name")]
+        [Display(Name = "Rating on 5 star scale")]
+        public string Name { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TravelReview/Controllers/Review.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+//added this model
+
+namespace TravelReview.Controllers
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public int Cost { get; set; }
+        public string Events { get; set; }        
+        public string Distance { get; set; }
+        
+
+        [ForeignKey("Category")]
+        public int CategotyID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
